6abd3599fb42a2c5cb34ec3eb243b091fe81ec3e
teria <>
2022-07-26 18:40:12 +0900

    メニュー追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import TodoList from "./components/TodoList";
 import {RecoilRoot} from "recoil";
+import {Menu} from "./components/Menu";
 
 
 function Home() {
@@ -15,24 +16,34 @@ function Contact() {
     return <h2>Contact</h2>;
 }
 
+function NotFound() {
+    return <h2>Not Found Page</h2>;
+}
+
 function App() {
   return (
       <BrowserRouter>
           <RecoilRoot>
               <div style={{ margin: '2em' }}>
                   <h1>Hello React Router</h1>
-                  <Route exact path="/">
-                      <Home />
-                  </Route>
-                  <Route path="/about">
-                      <About />
-                  </Route>
-                  <Route path="/contact">
-                      <Contact />
-                  </Route>
-                  <Route path="/todo">
-                      <TodoList />
-                  </Route>
+                  <Menu />
+                  <Switch>
+                      <Route exact path="/">
+                          <Home />
+                      </Route>
+                      <Route path="/about">
+                          <About />
+                      </Route>
+                      <Route path="/contact">
+                          <Contact />
+                      </Route>
+                      <Route path="/todo">
+                          <TodoList />
+                      </Route>
+                      <Route>
+                          <NotFound />
+                      </Route>
+                  </Switch>
               </div>
           </RecoilRoot>
       </BrowserRouter>
