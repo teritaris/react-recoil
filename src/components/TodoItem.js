f0479f49ff93a67e2ac4b3720752180b14ff6cf2
m-nodera <m-nodera@mercury-inc.cc>
2022-07-26 16:06:01 +0900

    完了/未完了表示切り替え追加

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,22 @@
 import { useRecoilState } from 'recoil';
 import { todoListState } from './atom';
 
-
 export const TodoItem = ({ item }) => {
     // getterとsetter？
     const [todoList, setTodoList] = useRecoilState(todoListState)
 
+    // 完了/未完了 状態を切り替える
+    const toggleItemCompletion = () => {
+        const index = todoList.findIndex((listItem) => listItem.id === item.id);
+        const newTodoList = [
+            ...todoList.slice(0, index), // 対象より前の要素たち
+            { ...item, isComplete: !item.isComplete }, // 対象の要素のisCompleteの現在状態をitemから拾ってT/F反転させる？
+            ...todoList.slice(index + 1) // 対象より後の要素
+        ];
+        setTodoList(newTodoList);
+    }
+
+    // 項目を削除する
     const deleteItem = () => {
         console.log('delete');
 
@@ -21,6 +32,9 @@ export const TodoItem = ({ item }) => {
 
     return (
         <div>
+            <button onClick={toggleItemCompletion}>
+                {item.isComplete ? '完' : '未'}
+            </button>
             {item.title}
             <span onClick={deleteItem} style={{ cursor:'pointer' }}>
                 X
