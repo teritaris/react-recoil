114984b89bb3e11e7f5f6807e6a518e60bd14120
m-nodera <m-nodera@mercury-inc.cc>
2022-07-26 18:40:12 +0900

    メニュー追加

---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+export const Menu = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/todo">Todo</a>
+                </li>
+            </ul>
+        </>
+    )
+}
