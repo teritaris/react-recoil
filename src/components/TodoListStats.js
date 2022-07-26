43450e7f43412339f8d6cf7ac5caedbea6ac1f9d
teria <>
2022-07-26 16:19:40 +0900

    完了数/未完了数を表示

--- src/components/TodoListStats.js
+++ src/components/TodoListStats.js
@@ -3,10 +3,12 @@ import { todoListStatsState } from "./selector";
 
 export const TodoListStats = () => {
     // Todoの数を取得するメソッドを実行して値を取得
-    const totalNum = useRecoilValue(todoListStatsState);
+    const { totalNum, totalCompletedNum, totalUncompletedNum } = useRecoilValue(todoListStatsState);
     return (
         <ul>
             <li>Todoの登録数:{totalNum}</li>
+            <li>完了数:{totalCompletedNum}</li>
+            <li>未完了数:{totalUncompletedNum}</li>
         </ul>
     );
 }
--- src/components/selector.js
+++ src/components/selector.js
@@ -2,12 +2,18 @@
 import {selector} from "recoil";
 import { todoListState } from './atom';
 
-// Todoの数を返す
+// Todoの数、完了数、未完了数を返す
 export const todoListStatsState = selector({
     key: 'todoListStatsState', // 一意にする必要ある atomのkeyともかぶってはいけないかは不明
     get: ({ get }) => {
         const todoList = get(todoListState);
         const totalNum = todoList.length; // todoListState配列の要素数を取得
-        return totalNum;
+        const totalCompletedNum = todoList.filter((item) => item.isComplete).length // 現在のリストからtrueを抽出した配列を作ってその長さを完了数とする
+        const totalUncompletedNum = totalNum - totalCompletedNum;
+        return {
+            totalNum,
+            totalCompletedNum,
+            totalUncompletedNum,
+        }
     },
 });
