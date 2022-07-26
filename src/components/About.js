958f9ac86faf2b0a3b37869f958ebfa1883c5d67
teria <>
2022-07-27 00:32:04 +0900

    AboutからContactへのリンクボタン追加

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,19 @@
-function About() {
-    return <h2>About</h2>;
+import { useNavigate } from 'react-router-dom';
+
+const About = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <h2>About</h2>
+            <button
+                onClick={() =>
+                    navigate('/contact')}
+            >
+                Contact
+            </button>
+        </>
+    )
 }
 
 export default About;
