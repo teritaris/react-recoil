dde8fcdc61808b55295204fd5d7f793f66693185
teria <>
2022-07-27 18:22:59 +0900

    posts一覧表示

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,33 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link, Outlet } from 'react-router-dom';
 
 function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await res.json();
+            setPosts(data);
+        };
+        fetchPosts();
+    }, []);
+
     return (
         <>
             <h2>Posts</h2>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Link to={`/posts/${post.id}`}>
+                            {post.id}:{post.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
             <Outlet />
         </>
-    )
+    );
 }
 
 export default Posts;
